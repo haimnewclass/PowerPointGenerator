5b3742b78574839c44175f6425a076ed9eb565ad
haim hod gilboa <haim@newclass.co.il>
2022-11-29 12:48:05 +0200

    Add project files.

---
+++ TestPowerPointGenerator/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using PowerPointGenerator;
+
+namespace TestPowerPointGenerator
+{
+    static class Program
+    {
+        static void Main()
+        {
+            var powerPointGenerator = new PowerPointFactory(Path.Combine(@"c:\a\ppt.pptx"));
+            var imageFilePaths = Directory.GetFiles(@"..\..\..\Alba", "*.jpg").ToList();
+             powerPointGenerator.CreateTitleAndImageSlides(imageFilePaths);
+            // powerPointGenerator.CreateTitleAndImageSlides
+           
+        }
+    }
+}
